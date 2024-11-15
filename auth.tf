d3efdaca3f25413357d5c05afdf90e51dbb49ebe
RobertsDBS <roberto.umana@databricks.com>
2024-11-14 19:11:51 -0600

    First Commit

---
+++ auth.tf
@@ -0,0 +1,5 @@
+provider "databricks" {
+  host  = "https://adb-3466794808232600.0.azuredatabricks.net/"
+  client_id = "b7172d83-2ffb-4488-a2ee-fb2f014d4b50"
+  client_secret = "dose36a73cb41c3bad7380976651800e1dfc"
+}
